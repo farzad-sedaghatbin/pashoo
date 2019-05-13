9eda4354ef94fec8ab72efbb1659347a22d7081a
farzad-sedaghatbin <farzad.sedahatbin@gmail.com>
2019-05-13 22:45:14 +0430

    add comment

--- src/main/java/ir/company/app/service/migmig/FarzadCommentResource.java
+++ src/main/java/ir/company/app/service/migmig/FarzadCommentResource.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/comments")
-public class CommentResource {
+public class FarzadCommentResource {
 
 
     @GetMapping(value = "{eventCode}")
